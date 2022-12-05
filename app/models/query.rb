182ad07c50e8f5d13e4da4e6f49b30a9708d637a
aliatayee <aliaqaatayee@gmail.com>
2022-12-05 14:14:02 +0430

    Create models

---
+++ app/models/query.rb
@@ -0,0 +1,14 @@
+class Query < ApplicationRecord
+  include PgSearch::Model
+
+  pg_search_scope :search_similarity,
+                  against: :query,
+                  using: {
+                    trigram: {
+                      threshold: 0.2
+                    }
+                  }
+  validates :query, presence: true
+  validates :query, length: { minimum: 3, maximum: 50 }
+  validates :user_id, presence: true
+end
